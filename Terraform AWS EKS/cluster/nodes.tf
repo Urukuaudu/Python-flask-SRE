7c8b1b81a67d52ac439413b90042c798f5f6f706
Urukuaudu <134658953+Urukuaudu@users.noreply.github.com>
2023-10-29 23:13:39 +0100

    Create nodes.tf

---
+++ Terraform AWS EKS/cluster/nodes.tf
@@ -0,0 +1,81 @@
+# Data block to read local vpc terraform.tfstate file
+data "terraform_remote_state" "network" {
+  backend = "local"
+  config = {
+    path = "../vpc-conf/terraform.tfstate"
+  }
+}
+
+# Create node group in the created vpc using created node role
+resource "aws_eks_node_group" "private-nodes" {
+  cluster_name    = aws_eks_cluster.demo.name
+  node_group_name = "private-nodes"
+  #node_role_arn   = data.terraform_remote_state.network.outputs.nodes_role
+  node_role_arn  = "arn:aws:iam::105410359089:role/eks-node-group-nodes"
+
+  subnet_ids = [
+    #aws_subnet.public[0.id,aws_subnet.public[1].id,
+    data.terraform_remote_state.network.outputs.private[0], data.terraform_remote_state.network.outputs.private[1]
+  ]
+  capacity_type  = "ON_DEMAND"
+  instance_types = ["t2.micro"]
+  
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 10
+    min_size     = 0
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  labels = {
+    role = "general"
+  }
+  # This tags are important if we are going to use an auto-scaler
+  tags = {
+    "k8s.io/cluster-autoscaler/demo"    = "owned"
+    "k8s.io/cluster-autoscaler/enabled" = true
+  }
+
+}
+
+/*
+# Create node group in the created vpc using created node role
+resource "aws_eks_node_group" "public-nodes" {
+  cluster_name    = aws_eks_cluster.demo.name
+  node_group_name = "public-nodes"
+  #node_role_arn   = data.terraform_remote_state.network.outputs.nodes_role
+  node_role_arn  = "arn:aws:iam::105410359089:role/eks-node-group-nodes"
+
+  subnet_ids = [
+    #aws_subnet.public[0.id,aws_subnet.public[1].id,
+    data.terraform_remote_state.network.outputs.public[0], data.terraform_remote_state.network.outputs.public[1]
+  ]
+  capacity_type  = "ON_DEMAND"
+  instance_types = ["t2.micro"]
+  
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 10
+    min_size     = 0
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  labels = {
+    role = "devops"
+  }
+  # This tags are important if we are going to use an auto-scaler
+  tags = {
+    "k8s.io/cluster-autoscaler/demo"    = "owned"
+    "k8s.io/cluster-autoscaler/enabled" = true
+  }
+
+}
+*/
