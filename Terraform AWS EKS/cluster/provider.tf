6b67642f2fbfecbf32d7ba5175144cbf745f7c1a
Urukuaudu <134658953+Urukuaudu@users.noreply.github.com>
2023-10-31 11:56:57 +0100

    Update provider.tf

--- Terraform AWS EKS/cluster/provider.tf
+++ Terraform AWS EKS/cluster/provider.tf
@@ -1,12 +1,14 @@
-provider "aws" {
-  region = "us-east-2"
-}
-
+# Terraform block
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.7.0"
+      version = "~> 5.0"
     }
   }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-west-2"
 }
