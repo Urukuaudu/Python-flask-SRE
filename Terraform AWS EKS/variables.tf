8bc5a117147664a71479c5cf7845753478e06110
Urukuaudu <134658953+Urukuaudu@users.noreply.github.com>
2023-10-31 11:51:35 +0100

    Update variables.tf

--- Terraform AWS EKS/variables.tf
+++ Terraform AWS EKS/variables.tf
@@ -1,6 +1,6 @@
 variable "private_cidr" {
   type    = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+  default = ["10.0.0.0/19", "10.0.32.0/19"]
 }
 variable "availability_zones" {
   type    = list(string)
@@ -9,5 +9,5 @@ variable "availability_zones" {
 
 variable "public_cidr" {
   type    = list(string)
-  default = ["10.0.3.0/24", "10.0.4.0/24"]
+  default = ["10.0.64.0/19", "10.0.96.0/19"]
 }
